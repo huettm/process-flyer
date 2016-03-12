cc33ca60f79058811716eae3222fd09373aabb7d
grobap <coder.peter.grobarcik@gmail.com>
2016-03-12 13:29:15 +0100

    Starting flyer UI.

--- src/main/java/com/olia/processflyer/client/ProcessFlyerScene.java
+++ src/main/java/com/olia/processflyer/client/ProcessFlyerScene.java
@@ -7,21 +7,14 @@ package com.olia.processflyer.client;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.resources.client.ClientBundle.Source;
-import com.google.gwt.resources.client.TextResource;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.olia.processflyer.shared.SceneUpdaterService;
 import com.olia.processflyer.shared.SceneUpdaterServiceAsync;
-import com.olia.processflyer.shared.bpmn.instance.HackathonProcessMock;
 import com.olia.processflyer.shared.bpmn.instance.impl.ProcessInstanceImpl;
 
 import thothbot.parallax.core.client.AnimatedScene;
@@ -37,7 +30,6 @@ import thothbot.parallax.core.shared.lights.DirectionalLight;
 import thothbot.parallax.core.shared.materials.Material;
 import thothbot.parallax.core.shared.materials.MeshBasicMaterial;
 import thothbot.parallax.core.shared.materials.MeshLambertMaterial;
-import thothbot.parallax.core.shared.materials.ShaderMaterial;
 import thothbot.parallax.core.shared.math.Color;
 import thothbot.parallax.core.shared.math.Vector3;
 import thothbot.parallax.core.shared.objects.Mesh;
@@ -49,12 +41,6 @@ import thothbot.parallax.core.shared.objects.Mesh;
 public class ProcessFlyerScene extends AnimatedScene {
 
 	PerspectiveCamera camera;
-
-	private String imageAsString;
-
-	private boolean moveup = true;
-
-	private static final String imageLocation = "Tulips.jpg";
 
 	FirstPersonControls controls;
 
@@ -76,9 +62,9 @@ public class ProcessFlyerScene extends AnimatedScene {
 		}
 	};
 
-	  Mesh meshLines;
-	   Mesh meshTris;
-	   Mesh meshMixed;
+	Mesh meshLines;
+	Mesh meshTris;
+	Mesh meshMixed;
 
 	@Override
 	protected void onStart() {
@@ -100,122 +86,42 @@ public class ProcessFlyerScene extends AnimatedScene {
 		double size = 150;
 
 		BoxGeometry geometryLines = new BoxGeometry(size, size, size);
-		BoxGeometry geometryTris = new BoxGeometry(size, size, size);
 
-		// this.controls = new FirstPersonControls(camera, getCanvas());
-		// this.controls.setMovementSpeed(1.0);
-		// this.controls.setLookSpeed(1.0);
+		getScene().add(new AmbientLight(0x404040));
 
-//		getScene().add(new AmbientLight(0x404040));
-//
-//		DirectionalLight light = new DirectionalLight(0xffffff);
-//		light.getPosition().set(0, 1, 0);
-//		getScene().add(light);
-//
+		DirectionalLight light = new DirectionalLight(0xffffff);
+		light.getPosition().set(0, 1, 0);
+		getScene().add(light);
+
 		MeshLambertMaterial material = new MeshLambertMaterial();
 		material.setAmbient(new Color(0xbbbbbb));
 		material.setSide(Material.SIDE.DOUBLE);
-//
-//		Map<String, List<ProcessBox>> processesMap = new HashMap<>();
 
-		// if(result != null) {
-		// for(ProcessInstanceImpl processInstance: result) {
-		// ProcessBox process = new ProcessBox();
-		// process.loadProcessDefinition(processInstance.getProcessTemplate());
-		// if(processesMap.containsKey(processInstance.getProcessTemplate().getName()))
-		// {
-		// processesMap.get(processInstance.getProcessTemplate().getName()).add(process);
-		// } else {
-		// List<ProcessBox> processesByName = new ArrayList<>();
-		// processesByName.add(process);
-		// processesMap.put(processInstance.getProcessTemplate().getName(),
-		// processesByName);
-		// }
-		// }
-		// }
+		MeshBasicMaterial materialLines = new MeshBasicMaterial();
+		materialLines.setWireframe(true);
 
-		// ProcessBox process = new ProcessBox();
-		// process.loadProcessDefinition(HackathonProcessMock.createTemplate());
-		// List<ProcessBox> p = new ArrayList<>();
-		// p.add(process);
-		// processesMap.put("Hackaton_Process", p);
-		// ProcessBox process2 = new ProcessBox();
-		// process2.loadProcessDefinition(HackathonProcessMock.createTemplate());
-		// processesMap.get("Hackaton_Process").add(process2);
-		//
-		// // Processlanes starten links von der Mitte und werden nach rechts
-		// // erweitert
-		// int processLaneXPosition = -1200;
-		// for (Entry<String, List<ProcessBox>> entry : processesMap.entrySet())
-		// {
-		// GWT.log("Displaying all processes for template: " + entry.getKey());
-		// int depth = 1000;
-		// for (ProcessBox processBox : entry.getValue()) {
-		// GWT.log("ProcessBox at depth " + depth);
-		// processBox.getPosition().add(new Vector3(processLaneXPosition, 0,
-		// depth));
-		// depth = depth + 100;
-		// for (VisualProcessObject processElement : processBox.processObjects)
-		// {
-		// Mesh currentMesh = new Mesh(processElement.getGeometry(), material);
-		// currentMesh.setPosition(processElement.getPosition().add(processBox.getPosition()));
-		// LOG.log(Level.FINER,"ProcessBox pos " + currentMesh.getPosition());
-		// getScene().add(currentMesh);
-		// }
-		// }
-		// processLaneXPosition = processLaneXPosition + 200;
-		// }
+		meshLines = new Mesh(geometryLines, materialLines);
+		getScene().add(meshLines);
 
-	     MeshBasicMaterial materialLines = new MeshBasicMaterial();
-	      materialLines.setWireframe(true);
-
-	      meshLines = new Mesh( geometryLines, materialLines );
-	      meshLines.getPosition().setX(-150);
-	      getScene().add( meshLines );
-
-	      // wireframe using gl.TRIANGLES (interpreted as triangles)
-
-	      Attribute attributesTris = new Attribute(Attribute.TYPE.V3, setupAttributes( geometryTris ));
-	      attributesTris.setBoundTo( Attribute.BOUND_TO.FACE_VERTICES );
-	      
-//	      ShaderMaterial materialTris = new ShaderMaterial( Resources.INSTANCE );
-//	      materialTris.getShader().addAttributes("center", attributesTris);
-
-	      meshTris = new Mesh( geometryTris, material );
-	      meshTris.getPosition().setX(150);
-	      getScene().add( meshTris );
-
-	      // wireframe using gl.TRIANGLES (mixed triangles and quads)
-
-	      SphereGeometry mixedGeometry = new SphereGeometry( size / 2.0, 32, 16 );
-
-	      Attribute attributesMixed = new Attribute(Attribute.TYPE.V3, setupAttributes( mixedGeometry ));
-	      attributesMixed.setBoundTo( Attribute.BOUND_TO.FACE_VERTICES );
-
-//	      ShaderMaterial materialMixed = new ShaderMaterial( Resources.INSTANCE );
-//	      materialMixed.getShader().addAttributes("center", attributesMixed);
-
-	      meshMixed = new Mesh( mixedGeometry, material );
-	      meshMixed.getPosition().setX(-150);
-	      getScene().add( meshMixed );
-
+		processSceneUpdate(result);
 	}
 
-	private List<List<Vector3>> setupAttributes( Geometry geometry) 
-	   {
-	      List<List<Vector3>> values = new ArrayList<List<Vector3>>();
-	      
-	      for( int f = 0; f < geometry.getFaces().size(); f ++ ) 
-	      {
-	         Face3 face = geometry.getFaces().get( f );
-	         values.add(f, Arrays.asList(
-	               new Vector3( 1, 0, 0 ), 
-	               new Vector3( 0, 1, 0 ), 
-	               new Vector3( 0, 0, 1 ) ));
-	      }
+	private void processSceneUpdate(ProcessInstanceImpl[] result2) {
+		// TODO Auto-generated method stub
+		
+	}
 
-	      return values;
-	   }
+	private List<List<Vector3>> setupAttributes(Geometry geometry) {
+		List<List<Vector3>> values = new ArrayList<List<Vector3>>();
+
+		for (int f = 0; f < geometry.getFaces().size(); f++) {
+			Face3 face = geometry.getFaces().get(f);
+			values.add(f, Arrays.asList(new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1)));
+		}
+
+		return values;
+	}
+
 	@Override
 	protected void onUpdate(double duration) {
 		// Called when the animation should be updated.
